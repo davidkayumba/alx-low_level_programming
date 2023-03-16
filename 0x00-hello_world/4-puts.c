0afe356c28b67049105cb213678d625af57c699a
davidkayumba <davidkayumba11@gmail.com>
2023-03-16 06:38:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#iclude <stio.h>
+/**
+ * main -A program that prints a line withs puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
