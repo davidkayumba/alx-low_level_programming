b0f126a17d7e3d2beb9886f92ea67ae5cd055226
davidkayumba <davidkayumba11@gmail.com>
2023-03-23 17:34:54 -0700

    Print line

--- 0x04-more_functions_nested_loops/6-main.c
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *print_line - draws a straight line according to parameter
- *@c: The number of line to drow
- *Return 0
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-print_line(0);
-print_line(2);
-print_line(10);
-print_line(-4);
-return (0);
+    print_line(0);
+    print_line(2);
+    print_line(10);
+    print_line(-4);
+    return (0);
 }
