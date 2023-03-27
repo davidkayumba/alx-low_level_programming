fc81184bbfb70d20ca20470d1eb370f541d586a4
davidkayumba <davidkayumba11@gmail.com>
2023-03-27 14:18:55 -0700

    My task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int howlong = 0, reverse;
+
+	while (*s != '\0')
+	{
+		howlong++;
+		s++;
+	}
+	s--;
+
+	for (reverse = howlong; reverse > 0; reverse--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
