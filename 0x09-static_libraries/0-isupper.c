ba0b1c3e3af128decf64e55b1c4c3bea24b1a6a5
davidkayumba <davidkayumba11@gmail.com>
2023-04-09 06:41:54 -0700

    all tasks

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,16 +1,15 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
- * _isupper - function to check the uppercase letter
+ * _isupper - function to check the uppercase latter
  * @c: the input to be checked
  *
- * Return: 1/0 depending on upper or lower case
+ * Return: 1/0 deppending on upper or lower case
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-		return (0);
+	return (c >= 'A' && c <= 'Z');
 }
