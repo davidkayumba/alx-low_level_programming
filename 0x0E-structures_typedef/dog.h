ad212a9fd1de706afa57145b4fa7e1aeb071e869
davidkayumba <davidkayumba11@gmail.com>
2023-04-18 08:30:39 -0700

    Task done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,7 +3,6 @@
 
 /**
  * struct dog - dog structure
- * @d: the dog to be initialized.
  * @name: Name of a dog
  * @age: Age of a dog
  * @owner: Dog's owner
@@ -15,8 +14,10 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 #endif
