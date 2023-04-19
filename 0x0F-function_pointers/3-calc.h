d27c5f2f25ffe7a0813a70af835fb7de174e91ab
davidkayumba <davidkayumba11@gmail.com>
2023-04-19 15:28:04 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -9,10 +9,11 @@
 typedef struct op
 {
 	char *op;
-	int (*f)(int 1, int 2);
-} op_t
+	int (*f)(int a, int b);
+}
+op_t
 
-int op_add(int a, int b);
+	int op_add(int a, int b);
 	int op_sub(int a, int b);
 	int op_mul(int a, int b);
 	int op_div(int a, int b);
