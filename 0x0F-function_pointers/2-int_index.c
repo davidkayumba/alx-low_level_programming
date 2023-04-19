624b5086b04be31f025a4f9c293c179d769c31a2
davidkayumba <davidkayumba11@gmail.com>
2023-04-19 13:05:15 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,13 +12,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array && cmp)
+	if (array == NULL ||size <= 0 || cmp == NULL)
+		return (-1);
+	
+	for (; i < size; i++)
 	{
-		for (; i < size; i++)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-		}
+		if (cmp(array[i]))
+			return (i);
 	}
 	return(-1);
 }
