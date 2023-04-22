bacd4906b4144a6c883f3c529bce3b9aff0050e0
davidkayumba <davidkayumba11@gmail.com>
2023-04-22 05:40:33 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,17 +9,12 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		;
-	else
 	{
-		if (d->name != NULL)
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: (nil)\n");
-		printf("Age: %f\n", d->age);
-		if (d->owner != NULL)
-			printf("owner: %s\n", d->owner);
-		else
-			printf("Owner:(nil)\n");
+		printf("NULL pointer passed to print_dog()\n");
+		return;
 	}
+
+	printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 }
